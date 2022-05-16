3a48ad23d87abd4aef2abdb569a64ff74d2756cf
Vijendra <docs.vijendra@gmail.com>
2022-05-17 05:02:48 +0530

    container for tags

---
+++ CLIENT/src/Components/TrendingTags/TrendingTags.jsx
@@ -0,0 +1,18 @@
+import './trendingTags.css'
+
+export const TrendingTags = () => {
+
+    return (
+        <div className="popular-tags">
+            <h3>Topics matching {"bussiness"}</h3>
+            <div>
+                <div><span>Bussiness</span></div>
+                <div><span>Bussiness Website</span></div>
+                <div><span>Bussinessvalues</span></div>
+                <div><span>Bussinessman</span></div>
+                <div><span>Bussinesstips</span></div>
+                <div><span>Bussiness Model</span></div>
+            </div>
+        </div>
+    )
+}
