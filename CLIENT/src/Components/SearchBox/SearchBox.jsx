0a627b0c87afe114e54a74fb6c0eb03240d8e72a
Vijendra <docs.vijendra@gmail.com>
2022-05-17 06:34:36 +0530

    inline style is removed

--- CLIENT/src/Components/SearchBox/SearchBox.jsx
+++ CLIENT/src/Components/SearchBox/SearchBox.jsx
@@ -1,11 +1,11 @@
-import { IconButton} from "@mui/material"
+import { IconButton } from "@mui/material"
 import SearchIcon from '@mui/icons-material/Search';
 import './searchBox.css'
 import { useRef, useState } from "react";
 
 export const SearchBox = () => {
 
-    const [searchText,setSearchText] = useState("")
+    const [searchText, setSearchText] = useState("")
     return (
         <>
             <div
@@ -14,9 +14,8 @@ export const SearchBox = () => {
                 <IconButton size="large" sx={{ ml: 2 }}>
                     <SearchIcon />
                 </IconButton>
-                <input 
+                <input
                     type="text"
-                    value={searchText}
                     placeholder="Search by tags" />
             </div>
         </>
