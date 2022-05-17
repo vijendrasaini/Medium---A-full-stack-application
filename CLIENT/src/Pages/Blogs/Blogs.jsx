fffd82357afa1c84be1b985fc188586a6fcc0af9
Vijendra <docs.vijendra@gmail.com>
2022-05-17 06:37:43 +0530

    blog page is sepetated.

---
+++ CLIENT/src/Pages/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import { AdminPanel } from "../../Components/AdminPanel/AdminPanel"
+import { BlogsContainer } from "../../Components/BlogsContainer/BlogsContainer"
+import { SearchAndTags } from "../../Components/SearchAndTags/SearchAndTags"
+import "./blogs.css"
+
+
+
+export const Blogs = ()=>{
+
+
+    return(
+        <div className="blogs-page">
+            <AdminPanel/>
+            <BlogsContainer/>
+            <SearchAndTags/>
+        </div>
+    )
+}
