ee55325ac71358879604eb80a7ca9091eecb7ac3
Vijendra <docs.vijendra@gmail.com>
2022-05-17 06:26:26 +0530

    container div element added to wrap up Admin panel

--- CLIENT/src/Components/AdminPanel/AdminPanel.jsx
+++ CLIENT/src/Components/AdminPanel/AdminPanel.jsx
@@ -13,29 +13,31 @@ export const AdminPanel = ()=>{
 
 
     return(
-        <div className="admin-panel">
-            <div className='admin-panel__medium-logo'>
-                <img src={logo} alt="logo image" />
-            </div>
-            <div className='admin-panel__icon-container'>
-                <IconButton>
-                    <HomeIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
-                </IconButton>
-                <IconButton>
-                    <NotificationsNoneIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
-                </IconButton>
-                <IconButton>
-                    <BookmarkBorderIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
-                </IconButton>
-                <IconButton>
-                    <ListAltIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
-                </IconButton>
-                <IconButton>
-                    <BorderColorIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
-                </IconButton>
-            </div>
-            <div className='admin-panel__user-profile-image'>
-                <Avatar src='vijendra' alt='user-image'/>
+        <div className='admin-panel-container'>
+            <div className="admin-panel">
+                <div className='admin-panel__medium-logo'>
+                    <img src={logo} alt="logo image" />
+                </div>
+                <div className='admin-panel__icon-container'>
+                    <IconButton>
+                        <HomeIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
+                    </IconButton>
+                    <IconButton>
+                        <NotificationsNoneIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
+                    </IconButton>
+                    <IconButton>
+                        <BookmarkBorderIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
+                    </IconButton>
+                    <IconButton>
+                        <ListAltIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
+                    </IconButton>
+                    <IconButton>
+                        <BorderColorIcon sx={{ fontSize : 32, color : "rgb(117 117 117)" }}/>
+                    </IconButton>
+                </div>
+                <div className='admin-panel__user-profile-image'>
+                    <Avatar src='vijendra' alt='user-image'/>
+                </div>
             </div>
         </div>
     )
