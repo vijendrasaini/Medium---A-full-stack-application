7c77eebc9d652400cc68e6e8dc4f4ad196f5c426
Vijendra <docs.vijendra@gmail.com>
2022-05-18 13:10:35 +0530

    added action creator for set a full blog

--- CLIENT/src/Redux/Blog/actioncreator.js
+++ CLIENT/src/Redux/Blog/actioncreator.js
@@ -1,11 +1,11 @@
 import { baseURL } from "../../Resources/universalData";
-import { SETBLOGS, SETTAGS } from "./action";
+import { SETBLOGS, SETFULLBLOG, SETTAGS } from "./action";
 
 
 
 export const setTags = (payload)=>({ type : SETTAGS, payload})
 export const setBlogs = (payload) => ({ type : SETBLOGS, payload})
-
+export const setFullBlog = (payload) => ( { type : SETFULLBLOG, payload})
 
 
 export const fetchBlogs = ()=>async function fetchBlogsSummeries(dispatch){
@@ -16,5 +16,16 @@ export const fetchBlogs = ()=>async function fetchBlogsSummeries(dispatch){
         dispatch(setBlogs(data))
     } catch (error) {
         console.log({message : error.message})   
+    }
+}
+
+export const fetchFullBlog = (blogId)=>async function fetchBlogById(dispatch){
+    const path = '/blog'
+    try {
+        const response = await fetch(`${baseURL}${path}/${blogId}`)
+        const data = await response.json()
+        dispatch(setFullBlog(data))
+    } catch (error) {
+        console.log({ message : error.message})
     }
 }
