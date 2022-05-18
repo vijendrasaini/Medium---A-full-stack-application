10ce4eb4bbde15639ec13add4676a18eb4dfaf94
Vijendra <docs.vijendra@gmail.com>
2022-05-18 13:10:54 +0530

    added blog state in store

--- CLIENT/src/Redux/Blog/reducer.js
+++ CLIENT/src/Redux/Blog/reducer.js
@@ -1,16 +1,22 @@
-import { SETBLOGS, SETTAGS } from "./action";
+import { SETBLOGS, SETFULLBLOG, SETTAGS } from "./action";
 
 
 const initState = {
     blogs : [],
+    blog : {},
     tags : []
 };
 export const reducer = (state = initState, { type, payload} ) =>{
     switch (type) {
         case SETBLOGS:
             return { ...state, blogs : payload};
+
+        case SETFULLBLOG:
+            return { ...state, blog : payload };
+
         case SETTAGS:
             return { ...state, tags : payload };
+
         default:
             return state
     }
