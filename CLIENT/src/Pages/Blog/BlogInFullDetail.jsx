f55fc77f91eb32492e8b16f0bce4dfcc01492e2c
Vijendra <docs.vijendra@gmail.com>
2022-05-19 09:47:19 +0530

    removed tags container

--- CLIENT/src/Pages/Blog/BlogInFullDetail.jsx
+++ CLIENT/src/Pages/Blog/BlogInFullDetail.jsx
@@ -1,6 +1,5 @@
 import { AdminPanel } from "../../Components/AdminPanel/AdminPanel"
 import { BlogInDetail } from "../../Components/Card/BlogInDetail"
-import { SearchAndTags } from "../../Components/SearchAndTags/SearchAndTags"
 import "./blog.css"
 
 
@@ -12,7 +11,6 @@ export const BlogInFullDetail = ()=>{
         <div className="blogs-page">
             <AdminPanel/>
             <BlogInDetail/>
-            <SearchAndTags/>
         </div>
     )
 }
