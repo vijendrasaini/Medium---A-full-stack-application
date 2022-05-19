18b49bd34ad71dc2848a44aab316da1b7ab25f67
Vijendra <docs.vijendra@gmail.com>
2022-05-19 13:58:36 +0530

    added conditional rendering for heading text

--- CLIENT/src/Components/BlogsContainer/BlogsContainer.jsx
+++ CLIENT/src/Components/BlogsContainer/BlogsContainer.jsx
@@ -3,6 +3,7 @@ import { BlogCard } from '../Card/BlogCard'
 import "./blogsContainer.css"
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchBlogs } from '../../Redux/Blog/actioncreator'
+import { TrendingTags } from '../TrendingTags/TrendingTags'
 
 
 export const BlogsContainer = () => {
@@ -16,23 +17,25 @@ export const BlogsContainer = () => {
     },[])
     
     return (
-        <div className='blogs-container'>
-            <div>
-                { searchKeyword != "" && <h1>Results for { searchKeyword } ...</h1>}
-                {   
-                    blogs.length ? 
-                    blogs?.map(blog => <BlogCard 
-                        key={blog._id}
-                        { ...blog}
-                    />)
-                    :
-                    <div>
-                        <img style={{
-                            width : "100%",
-                            height : "400px"
-                        }} src="https://cdn.dribbble.com/users/746538/screenshots/7069080/no-results_4x.png" alt="" />
-                    </div>
-                }
+        <div className="blogs-page">
+            <div className='blogs-container'>
+                <div>
+                    { searchKeyword != "" && <h1>Results for { searchKeyword } ...</h1>}
+                    {   
+                        blogs.length ? 
+                        blogs?.map(blog => <BlogCard 
+                            key={blog._id}
+                            { ...blog}
+                        />)
+                        :
+                        <div className='blogs-container__no-results'>
+                            <img src="https://cdn.dribbble.com/users/746538/screenshots/7069080/no-results_4x.png" alt="" />
+                        </div>
+                    }
+                </div>
+            </div>
+            <div className='blogs-page__tags'>
+                <TrendingTags/>
             </div>
         </div>
     )
