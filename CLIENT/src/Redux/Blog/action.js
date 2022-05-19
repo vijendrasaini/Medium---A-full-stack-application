b97f474597e569e9b4141f84524c6e86b6568cff
Vijendra <docs.vijendra@gmail.com>
2022-05-19 09:48:08 +0530

    added an action for search keyword

--- CLIENT/src/Redux/Blog/action.js
+++ CLIENT/src/Redux/Blog/action.js
@@ -1,3 +1,5 @@
 export const SETBLOGS = "SETBLOGS"
 export const SETTAGS = "SETTAGS"
 export const SETFULLBLOG = "SETFULLBLOG" 
+export const SETSEARCHTAG = "SETSEARCHTAG"
+export const SETSEARCHKEYWORD = "SETSEARCHKEYWORD"
