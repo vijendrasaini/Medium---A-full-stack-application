0edba3226f52ef7aeed3405ef11e369368f948a4
Vijendra <docs.vijendra@gmail.com>
2022-05-19 12:33:00 +0530

    removed container div effect

--- CLIENT/src/Pages/Blog/BlogInFullDetail.jsx
+++ CLIENT/src/Pages/Blog/BlogInFullDetail.jsx
@@ -1,16 +1,16 @@
-import { AdminPanel } from "../../Components/AdminPanel/AdminPanel"
-import { BlogInDetail } from "../../Components/Card/BlogInDetail"
-import "./blog.css"
+// import { AdminPanel } from "../../Components/AdminPanel/AdminPanel"
+// import { BlogInDetail } from "../../Components/Card/BlogInDetail"
+// import "./blog.css"
 
 
 
-export const BlogInFullDetail = ()=>{
+// export const BlogInFullDetail = ()=>{
 
 
-    return(
-        <div className="blogs-page">
-            <AdminPanel/>
-            <BlogInDetail/>
-        </div>
-    )
-}
+//     return(
+//         <div className="blogs-page">
+//             {/* <AdminPanel/> */}
+//             <BlogInDetail/>
+//         </div>
+//     )
+// }
