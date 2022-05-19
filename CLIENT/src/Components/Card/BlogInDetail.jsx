4129f3490742b38a97ebce512a464a32de43e315
Vijendra <docs.vijendra@gmail.com>
2022-05-19 12:31:26 +0530

    user-card is added.

--- CLIENT/src/Components/Card/BlogInDetail.jsx
+++ CLIENT/src/Components/Card/BlogInDetail.jsx
@@ -4,71 +4,78 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import IosShareIcon from '@mui/icons-material/IosShare';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import './blogInDetail.css'
-import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchFullBlog } from "../../Redux/Blog/actioncreator";
+import { SearchBox } from "../SearchBox/SearchBox";
 
 export const BlogInDetail = () => {
 
     const { blogId } = useParams()
     const dispatch = useDispatch()
-    const { blog} = useSelector(store=> store)
-    useEffect(()=>{
+    const { blog } = useSelector(store => store)
+    useEffect(() => {
         dispatch(fetchFullBlog(blogId))
-    },[])
-    
+    }, [])
+
     return (
-        <div className="blogs-container">
-            <div className="blog-content">
-                <div className="blog-content__header">
-                    <div>
-                        <div className="blog-content__user-image">
-                            <img src="https://miro.medium.com/fit/c/60/60/0*c-sebS2TCXzg_7rS" alt="" />
+        <>
+            <div className="blogs-container">
+                <div className="blog-content">
+                    <div className="blog-content__header">
+                        <div>
+                            <div className="blog-content__user-image">
+                                <img src={blog?.user?.avatar} alt={blog?.user?.name} />
+                            </div>
+                            <div>
+                                <div>
+                                    <span className="blog-content__user-name">{blog?.user?.name}</span>
+                                    <button className="blog-content__follow-btn">Follow</button>
+                                </div>
+                                <div>
+                                    <span>Dec 2, 2021</span>
+                                    <span> . {8} min read</span>
+                                </div>
+                            </div>
                         </div>
                         <div>
-                            <div>
-                                <span className="blog-content__user-name">Benjamin Hardy, PhD</span>
-                                <button className="blog-content__follow-btn">Follow</button>
-                            </div>
-                            <div>
-                                <span>Dec 2, 2021</span>
-                                <span> . 8 min read</span>
-                            </div>
+                            <IconButton>
+                                <BookmarkBorderIcon />
+                            </IconButton>
+                            <IconButton>
+                                <MoreHorizIcon />
+                            </IconButton>
                         </div>
                     </div>
-                    <div>
-                        <IconButton>
-                            <BookmarkBorderIcon />
-                        </IconButton>
-                        <IconButton>
-                            <MoreHorizIcon />
-                        </IconButton>
+                    <div className="blog-content__HTML-Content" dangerouslySetInnerHTML={{ __html: blog?.htmlContent }}>
                     </div>
-                </div>
-                <div className="blog-content__HTML-Content" dangerouslySetInnerHTML={{__html : blog?.htmlContent}}>
-                </div>
-                <div className="blog-content__footer">
-                    <div>
-                        <IconButton>
-                            <ChatBubbleOutlineIcon />
-                        </IconButton>
-                        <span>Responses</span>
-                    </div>
-                    <div>
-                        <IconButton>
-                            <IosShareIcon />
-                        </IconButton>
-                        <IconButton>
-                            <BookmarkBorderIcon />
-                        </IconButton>
-                        <IconButton>
-                            <MoreHorizIcon />
-                        </IconButton>
+                    <div className="blog-content__footer">
+                        <div>
+                            <IconButton>
+                                <ChatBubbleOutlineIcon />
+                            </IconButton>
+                            <span>Responses</span>
+                        </div>
+                        <div>
+                            <IconButton>
+                                <IosShareIcon />
+                            </IconButton>
+                            <IconButton>
+                                <BookmarkBorderIcon />
+                            </IconButton>
+                            <IconButton>
+                                <MoreHorizIcon />
+                            </IconButton>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className="user-card">
+                {/* <SearchBox/> */}
+                <img src={blog?.user?.avatar} alt={blog?.user?.name} />
+                <h3>{blog?.user?.name}</h3>
+            </div>
+        </>
     );
 }
