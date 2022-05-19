5ab2edfa233fe7e6d07ea053dd64b4e27f0ceb03
Vijendra <docs.vijendra@gmail.com>
2022-05-19 14:58:16 +0530

    added a span tag for readtime vare

--- CLIENT/src/Components/Card/BlogCard.jsx
+++ CLIENT/src/Components/Card/BlogCard.jsx
@@ -2,15 +2,16 @@ import { Avatar } from '@mui/material'
 import { Link } from 'react-router-dom'
 import './blogcard.css'
 
-export const BlogCard = ({ blog, _id }) => {
+export const BlogCard = ({ blog, _id, user }) => {
 
     return (
         <div className="blog-overview">
             <div className='blog-overview__user-and-time'>
-                <Avatar sx={{ height: 30, width: 30 }} src='https://miro.medium.com/fit/c/60/60/0*c-sebS2TCXzg_7rS' alt='blog-user' />
+                <Avatar sx={{ height: 30, width: 30 }} src={user?.avatar} alt={user?.name} />
                 <div>
-                    <span>{"Vjiendra Saini"}</span>
+                    <span>{user?.name}</span>
                     <span> . {"5 days ago"}</span>
+                    <span> . {"2 min read"}</span>
                 </div>
             </div>
             <Link to={`${"username"}/${_id}`}
@@ -22,7 +23,7 @@ export const BlogCard = ({ blog, _id }) => {
                             <h2>{ blog?.title }</h2>
                             <p>{blog?.body + " ..."}</p>
                         </div>
-                        <div>
+                        {/* <div>
                             <div className='blog-overview__tags'>
                                 <small>{"Self Improvement"}</small>
                                 <div>
@@ -30,7 +31,7 @@ export const BlogCard = ({ blog, _id }) => {
                                     <small>  .  {"Selected for you"}</small>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                     <div>
                         <img src="https://miro.medium.com/fit/c/140/140/0*PjkE1iOltIbO7wgO" alt="blog-image" />
