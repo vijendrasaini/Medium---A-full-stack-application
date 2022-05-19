bed9e2f2bcfcbbeb24861f81847928f77a03e26f
Vijendra <docs.vijendra@gmail.com>
2022-05-19 12:27:36 +0530

    modified to implement common searchbar

--- CLIENT/src/Components/AllRouters/AllRouters.jsx
+++ CLIENT/src/Components/AllRouters/AllRouters.jsx
@@ -1,16 +1,42 @@
 import { Route, Routes } from 'react-router-dom'
-import { BlogInFullDetail } from '../../Pages/Blog/BlogInFullDetail'
-import { Blogs } from '../../Pages/Blogs/Blogs'
-
+import { AdminPanel } from '../AdminPanel/AdminPanel'
+import { BlogsContainer } from '../BlogsContainer/BlogsContainer'
+import { BlogInDetail } from '../Card/BlogInDetail'
+import { SearchBox } from '../SearchBox/SearchBox'
+import "./routers.css"
 
 
 export const AllRouters = ()=>{
 
 
     return(
-        <Routes>
-            <Route path='/' element={<Blogs/>}/>
-            <Route path='/:username/:blogId' element={<BlogInFullDetail/>}/>
-        </Routes>
+        <div>
+            <AdminPanel/>
+            <div className='routes'>
+                <Routes>
+                    <Route path='/' element={<BlogsContainer/>}/>
+                    <Route path='/:username/:blogId' element={<BlogInDetail/>}/>
+                </Routes>
+                <SearchBox/>
+            </div>
+        </div>
     )
 }
+
+
+// import { Route, Routes } from 'react-router-dom'
+// import { BlogInFullDetail } from '../../Pages/Blog/BlogInFullDetail'
+// import { Blogs } from '../../Pages/Blogs/Blogs'
+
+
+
+// export const AllRouters = ()=>{
+
+
+//     return(
+//         <Routes>
+//             <Route path='/' element={<Blogs/>}/>
+//             <Route path='/:username/:blogId' element={<BlogInFullDetail/>}/>
+//         </Routes>
+//     )
+// }
