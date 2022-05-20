5a6bf06303f13e64c1cd027dc290a713b920d382
Vijendra <docs.vijendra@gmail.com>
2022-05-20 19:12:17 +0530

    removed some comments

--- CLIENT/src/Components/AllRouters/AllRouters.jsx
+++ CLIENT/src/Components/AllRouters/AllRouters.jsx
@@ -290,21 +290,3 @@ export const AllRouters = () => {
         </>
         )
 }
-
-
-// import { Route, Routes } from 'react-router-dom'
-// import { BlogInFullDetail } from '../../Pages/Blog/BlogInFullDetail'
-// import { Blogs } from '../../Pages/Blogs/Blogs'
-
-
-
-// export const AllRouters = ()=>{
-
-
-//     return(
-//         <Routes>
-//             <Route path='/' element={<Blogs/>}/>
-//             <Route path='/:username/:blogId' element={<BlogInFullDetail/>}/>
-//         </Routes>
-//     )
-// }
