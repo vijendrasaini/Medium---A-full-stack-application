d9a2679d49fe87ccd8024f1c4e750444ed5ba80d
Vijendra <docs.vijendra@gmail.com>
2022-05-20 19:36:22 +0530

    add 2 states in auth init state

--- CLIENT/src/Redux/Auth/actioncreator.js
+++ CLIENT/src/Redux/Auth/actioncreator.js
@@ -1,9 +1,11 @@
 import { baseURL } from "../../Resources/universalData";
-import { AUTH_STATUS, LOGGED_USER } from "./action";
+import { AUTH_STATUS, LOGGED_USER, OPEN_SIGNIN_POPUP, SING_IN_ALERT } from "./action";
 
 
 
 export const setAuthStatus = (payload)=>({ type : AUTH_STATUS, payload})
 export const setLoggedUser = (payload) => ({ type : LOGGED_USER, payload})
+export const openSignInPopup = (payload) => ({ type : OPEN_SIGNIN_POPUP, payload})
+export const setSignInAlert = () => ({ type : SING_IN_ALERT})
 
 
--- CLIENT/src/Redux/Auth/reducer.js
+++ CLIENT/src/Redux/Auth/reducer.js
@@ -1,9 +1,11 @@
-import { AUTH_STATUS, LOGGED_USER} from "./action";
+import { AUTH_STATUS, LOGGED_USER, OPEN_SIGNIN_POPUP, SING_IN_ALERT} from "./action";
 
 
 const initState = {
     authStatus : localStorage.getItem('user') ? true : false,
-    loggedUser : JSON.parse(localStorage.getItem('user')) || null
+    loggedUser : JSON.parse(localStorage.getItem('user')) || null,
+    signInAlert : true,
+    openSignInPopUp : false
 };
 export const reducer = (state = initState, { type, payload} ) =>{
     switch (type) {
@@ -12,6 +14,12 @@ export const reducer = (state = initState, { type, payload} ) =>{
 
         case LOGGED_USER:
             return { ...state, loggedUser : payload}
+
+        case SING_IN_ALERT:
+            return { ...state, signInAlert : false}
+
+        case OPEN_SIGNIN_POPUP:
+            return { ...state, openSignInPopUp : payload}
 
         default:
             return state
