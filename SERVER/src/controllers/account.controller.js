4eb87cd2c337d32bed2678f6a1beb63a5534171b
Vijendra <docs.vijendra@gmail.com>
2022-05-20 17:41:22 +0530

    modified user data to be saved.

--- SERVER/src/controllers/account.controller.js
+++ SERVER/src/controllers/account.controller.js
@@ -1,65 +1,69 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user.model')
 
-const newToken = (user)=>{
-    return jwt.sign({ user}, "admin@media")
+const newToken = (user) => {
+    return jwt.sign({ user }, "admin@media")
 }
 
-const signup = async (req, res, next)=>{
+const signup = async (req, res, next) => {
     try {
-        let user = await User.findOne({username}).lean().exec()
-        if(user)
+        let user = await User.findOne({ email: req.body.email }).lean().exec()
+        if (user)
             return res
-            .status(400)
-            .send(
-                {
-                    status : "failure",   
-                    reason : "User already exits"
-                }
-            )
-        console.log(req.body)
-        return res.send({ status : "success"})
-        user = await User.create(req.body)
+                .status(400)
+                .send(
+                    {
+                        status: "failure",
+                        reason: "User already exits"
+                    }
+                )
+        user = await User.create({
+            name: req.body.name,
+            username: req.body.email.split("@")[0],
+            email: req.body.email,
+            password: req.body.password
+        })
         return res
-        .status(201)
-        .send({ status : 'success'})
+            .status(201)
+            .send({ status: 'success' })
     } catch (error) {
+        console.log({ message: error.message })
         return res
-        .status(201)
-        .send({ status : 'success'})
+            .status(201)
+            .send({ status: 'failure' })
     }
 }
 
-const signin = async (req, res, next)=>{
-    const user = await User.findOne({ email : req.body.email})
-    if(user)
+const signin = async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email })
+    if (user)
         return res
-        .status(404)
-        .send(
-            {
-                status : "failure",
-                reason : 'User not found'
-            }
-        )
-        const passwordMatch = user.checkPassword(req.body.password)
-        if(!passwordMatch)
-            return res
             .status(404)
             .send(
                 {
-                    status : "failure",
-                    reason : 'Credentails are wrong.'
+                    status: "failure",
+                    reason: 'User not found'
                 }
             )
-            const token = newToken(user)
+    const passwordMatch = user.checkPassword(req.body.password)
+    if (!passwordMatch)
         return res
+            .status(404)
+            .send(
+                {
+                    status: "failure",
+                    reason: 'Credentails are wrong.'
+                }
+            )
+    const token = newToken(user)
+    return res
         .status(200)
         .send(
             {
-                status : "success",
+                status: "success",
                 token
             }
         )
 }
 
-module.exports = { signin, signup, newToken}
+module.exports = { signin, signup, newToken }
