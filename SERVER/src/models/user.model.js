910101ba07ca84470982ed06daa342394ddb3743
Vijendra <docs.vijendra@gmail.com>
2022-05-20 17:41:59 +0530

    added password, email field

--- SERVER/src/models/user.model.js
+++ SERVER/src/models/user.model.js
@@ -2,9 +2,11 @@ const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema(
     {
-        username: { type : String, required : true},
         name: { type : String, required : true},
-        avatar : { type : String}
+        username: { type : String, required : true},
+        email: { type : String, required : true},
+        password: { type : String, required : true},
+        avatar : { type : String, default : "https://via.placeholder.com/200"}
     },
     {
         versionKey : false
