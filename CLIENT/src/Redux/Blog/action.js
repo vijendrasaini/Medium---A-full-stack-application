45cfda485297effef4d88fc2eea38bc36c5f68cd
Vijendra <docs.vijendra@gmail.com>
2022-05-21 07:45:06 +0530

    added page and totaldocs actions

--- CLIENT/src/Redux/Blog/action.js
+++ CLIENT/src/Redux/Blog/action.js
@@ -3,4 +3,8 @@ export const SETTAGS = "SETTAGS"
 export const SETFULLBLOG = "SETFULLBLOG" 
 export const SETSEARCHTAG = "SETSEARCHTAG"
 export const SETSEARCHKEYWORD = "SETSEARCHKEYWORD"
+
 export const LOADING = "LOADING"
+
+export const PAGE = "PAGE"
+export const TOTAL_DOCS = "TOTAL_DOCS"
