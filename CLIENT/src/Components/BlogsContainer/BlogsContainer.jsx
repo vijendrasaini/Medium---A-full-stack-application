73a662369a0231a4d58d32120d8c21ce39cbfd25
Vijendra <docs.vijendra@gmail.com>
2022-05-21 07:42:41 +0530

    added page state

--- CLIENT/src/Components/BlogsContainer/BlogsContainer.jsx
+++ CLIENT/src/Components/BlogsContainer/BlogsContainer.jsx
@@ -4,20 +4,22 @@ import "./blogsContainer.css"
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchBlogs } from '../../Redux/Blog/actioncreator'
 import { TrendingTags } from '../TrendingTags/TrendingTags'
+import { NextandPrebtnBox } from '../Pagination/NextandPrebtnBox'
 
 
 export const BlogsContainer = () => {
 
     
-    const {blogs, searchKeyword } = useSelector(store=> store.blog)
+    const {blogs, searchKeyword, page } = useSelector(store=> store.blog)
     const dispatch = useDispatch()
     useEffect(()=>{
-        dispatch(fetchBlogs(searchKeyword))
+        dispatch(fetchBlogs(searchKeyword, page))
     },[])
     
     return (
         <div className="blogs-page">
             <div className='blogs-container'>
+                <div>
                 <div>
                     { searchKeyword != "" && <h1>Results for { searchKeyword } ...</h1>}
                     {   
@@ -31,6 +33,8 @@ export const BlogsContainer = () => {
                             <img src="https://cdn.dribbble.com/users/746538/screenshots/7069080/no-results_4x.png" alt="" />
                         </div>
                     }
+                </div>
+                <NextandPrebtnBox/>
                 </div>
             </div>
             <div className='blogs-page__tags'>
